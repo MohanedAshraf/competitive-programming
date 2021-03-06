77effb943c2cc4791c16d823fae7d51e89ea89b2
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2021-03-06 05:40:12 +0200

    leetcode 217

---
+++ solutions/leetcode/217. Contains Duplicate.cpp
@@ -0,0 +1,12 @@
+static int __=[](){std::ios::sync_with_stdio(false);return 1000;}();
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> s ;
+        for(int i = 0 ; i<nums.size() ; i++)
+            s.insert(nums[i]);
+            
+            
+        return s.size() != nums.size();
+    }
+};
