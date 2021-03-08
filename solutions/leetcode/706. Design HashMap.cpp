0160c7ed2b6e51e6516318867ea0223575e5a467
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2021-03-08 05:41:36 +0200

    leetcode 706

---
+++ solutions/leetcode/706. Design HashMap.cpp
@@ -0,0 +1,16 @@
+class MyHashMap {
+public:
+     int arr[1000001];
+    MyHashMap() {
+        memset(arr, -1 , sizeof(arr));
+    }
+    void put(int key, int val) {
+        arr[key] = val;
+    }
+    int get(int key) {
+        return arr[key];
+    }
+    void remove(int key) {
+        arr[key] = -1;
+    }
+};
