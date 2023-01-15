bb057d7e90e1a86cf922a201ff826755cd07aeec
Mohaned Ashraf <33457145+MohanedAshraf@users.noreply.github.com>
2023-01-15 16:24:43 +0200

    leetcode 344

--- solutions/leetcode/344. Reverse String.cpp
+++ solutions/leetcode/344. Reverse String.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        reverse(s.begin() , s.end());
+        int j = s.size() -1 ;
+        for(int i = 0 ; i < s.size() && i<j ; i++){
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j--] = tmp;
+        }
     }
 };
